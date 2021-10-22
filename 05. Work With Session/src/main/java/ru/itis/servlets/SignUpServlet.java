1757a2a540f929afac7c423ce12c021165eee6e4
MStolpovskiy <mstolpovskym@gmail.com>
2021-10-23 01:27:06 +0300

    0.1

---
+++ 05. Work With Session/src/main/java/ru/itis/servlets/SignUpServlet.java
@@ -0,0 +1,46 @@
+package ru.itis.servlets;
+
+import org.springframework.context.ApplicationContext;
+import ru.itis.dto.SignUpForm;
+import ru.itis.services.SignUpService;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/sign-up")
+public class SignUpServlet extends HttpServlet {
+
+    private SignUpService signUpService;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        ServletContext servletContext = config.getServletContext();
+        ApplicationContext applicationContext = (ApplicationContext) servletContext.getAttribute("springContext");
+        this.signUpService = applicationContext.getBean(SignUpService.class);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/jsp/signUp.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SignUpForm signUpForm = SignUpForm.builder()
+                .firstName(request.getParameter("firstName"))
+                .lastName(request.getParameter("lastName"))
+                .email(request.getParameter("email"))
+                .password(request.getParameter("password"))
+                .build();
+
+        this.signUpService.signUp(signUpForm);
+
+        response.sendRedirect("/sign-in");
+    }
+}
