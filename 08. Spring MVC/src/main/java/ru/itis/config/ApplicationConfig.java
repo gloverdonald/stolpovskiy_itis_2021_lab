fb2914a25a0ae229eae484b6ebcec7ae5d81181b
MStolpovskiy <mstolpovskym@gmail.com>
2021-11-20 21:59:37 +0300

    0.1

---
+++ 08. Spring MVC/src/main/java/ru/itis/config/ApplicationConfig.java
@@ -0,0 +1,56 @@
+package ru.itis.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("ru.itis")
+@PropertySource("classpath:application.properties")
+public class ApplicationConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource(HikariConfig config) {
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
+        viewResolver.setPrefix("");
+        viewResolver.setSuffix(".ftlh");
+        viewResolver.setContentType("text/html;charset=UTF-8");
+        return viewResolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfig() {
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPath("/templates/");
+        return configurer;
+    }
+
+    @Bean
+    public HikariConfig hikariConfig() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(environment.getProperty("db.url"));
+        config.setUsername(environment.getProperty("db.user"));
+        config.setPassword(environment.getProperty("db.password"));
+        config.setDriverClassName(environment.getProperty("db.driver"));
+        config.setMaximumPoolSize(Integer.parseInt(environment.getProperty("db.hikari.pool-size")));
+        return config;
+    }
+}
