8d67cb058c9f34f42fb9a7389f7a8f2d63e9c794
Mikhail <mstolpovskym@gmail.com>
2022-04-19 23:59:39 +0300

    fix handler

--- 20. Validation/src/main/java/ru/itis/blog/validation/handlers/ValidationExceptionHandler.java
+++ 20. Validation/src/main/java/ru/itis/blog/validation/handlers/ValidationExceptionHandler.java
@@ -1,8 +1,10 @@
 package ru.itis.blog.validation.handlers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.itis.blog.validation.http.ValidationErrorDto;
 import ru.itis.blog.validation.http.ValidationExceptionResponse;
@@ -13,6 +15,7 @@ import java.util.List;
 @RestControllerAdvice
 public class ValidationExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ValidationExceptionResponse handleException(MethodArgumentNotValidException exception) {
         List<ValidationErrorDto> errors = new ArrayList<>();
